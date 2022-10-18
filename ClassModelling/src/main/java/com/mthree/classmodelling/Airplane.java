8f8078bdca12d569d6555d248f952685da51a347
ash-g773 <aishwaryageorge@ymail.com>
2022-10-18 10:21:49 +0100

    initial test commit to class + objects git

---
+++ ClassModelling/src/main/java/com/mthree/classmodelling/Airplane.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mthree.classmodelling;
+
+/**
+ *
+ * @author ash_7
+ */
+public class Airplane {
+    
+    public String carrier;
+    public String flightNumber;
+    
+    private String destination;
+    private String origin;
+    
+    
+}
