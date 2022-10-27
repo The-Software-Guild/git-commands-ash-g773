f635df1a46e709a62d1e5bd43588b708963bde34
ash-g773 <aishwaryageorge@ymail.com>
2022-10-27 15:53:23 +0100

    ALittleChaos

--- PracticeProgrammingBasics/src/main/java/com/sg/foundations/flowcontrol/random/ALittleChaos.java
+++ PracticeProgrammingBasics/src/main/java/com/sg/foundations/flowcontrol/random/ALittleChaos.java
@@ -5,10 +5,35 @@
  */
 package com.sg.foundations.flowcontrol.random;
 
+import java.util.Random;
+
 /**
  *
  * @author ash_7
  */
 public class ALittleChaos {
     
+    public static void main(String[] args) {
+        
+        Random randomizer = new Random();
+        
+        System.out.println("Random can make integers: " + randomizer.nextInt());
+        System.out.println("Or a double: " + randomizer.nextDouble());
+        System.out.println("Or even a boolean: " + randomizer.nextBoolean());
+        
+        int num = randomizer.nextInt(100);
+        
+        System.out.println("Your can store a randomized result: " + num);
+        System.out.println("And use it over and over again: " + num + ", " + num);
+        
+        System.out.println("Or just keep generating new values");
+        System.out.println("Here's a bunch of numbers from 0-100: ");
+        
+        System.out.print((randomizer.nextInt(51) + 50) + ", "); //these three will always be above 50
+        System.out.print((randomizer.nextInt(51) + 50) + ", ");
+        System.out.print((randomizer.nextInt(51) + 50) + ", ");
+        System.out.print(randomizer.nextInt(101) + ", ");
+        System.out.print(randomizer.nextInt(101) + ", ");
+        System.out.println(randomizer.nextInt(101));
+    } 
 }
